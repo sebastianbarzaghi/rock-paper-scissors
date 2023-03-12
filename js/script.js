7eb5024f5ca03e4be70ec53c00a2c97f2ff1e527
sebastianbarzaghi <sebastian.barzaghi@outlook.com>
2023-03-12 13:03:27 +0100

    fix: clean HTML and JS code and add comments

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
+// define possible options for computer choice and other variables
 const options = ["Rock", "Paper", "Scissors"];
+let wins = 0;
+let losses = 0;
+let ties = 0;
 
+// get document sections
 const computerChoiceMade = document.getElementById("computerChoice");
 const playerChoiceMade = document.getElementById("playerChoice");
 const singleGameResults = document.getElementById("singleGameResults");
 const globalGameResults = document.getElementById("globalGameResults");
 
+// get computer choice and return it
 function getComputerChoice() {
     const randomIndex = Math.floor(Math.random() * options.length);
     const selectedOption = options[randomIndex];
     return selectedOption
 }
 
-let wins = 0;
-let losses = 0;
-let ties = 0;
-
+// main function that starts and manages game logic, called when
+// clicking on #playerButton
 function playGame() {
     let computerChoice = getComputerChoice();
     computerChoiceMade.innerHTML = `Computer: ${computerChoice}`;
@@ -26,14 +30,18 @@ function playGame() {
     if (computerChoice == playerChoice) {
         singleGameResults.innerHTML = "It's a tie!";
         ties++;
-    } else if ((computerChoice == "Rock" && playerChoice == "Scissors")
-                || (computerChoice == "Scissors" && playerChoice == "Paper")
-                || (computerChoice == "Paper" && playerChoice == "Rock")) {
-        singleGameResults.innerHTML = "You lost!";
-        losses++;
+    } else if ((computerChoice == "Rock" && 
+        playerChoice == "Scissors") ||
+        (computerChoice == "Scissors" && 
+        playerChoice == "Paper") ||
+        (computerChoice == "Paper" && 
+        playerChoice == "Rock")) {
+            singleGameResults.innerHTML = "You lost!";
+            losses++;
     } else {
         singleGameResults.innerHTML = "You won!";
         wins++;
     }
+    
     globalGameResults.innerHTML = `Wins: ${wins} - Losses: ${losses} - Ties: ${ties}`;
 }
